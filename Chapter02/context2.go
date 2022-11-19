c8ef074a9f3c77c7bd73967ebf408e5b5c32b175
hide0621 <fujihara_hideyuki@retail-ai.jp>
2022-11-20 08:22:15 +0900

    contextパッケージを別の教材で学習

---
+++ Chapter02/context2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	// contextパッケージの使用開始の宣言
+	ctx := req.Context()
+
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+
+	// サーバーが処理をさばいてレスポンスを返すところまで完了した
+	case <-ctx.Done():
+	}
+
+	// サーバーがレスポンスをさばけなくて処理が終わった
+	err := ctx.Err()
+
+	fmt.Println("server:", err)
+	internalError := http.StatusInternalServerError
+	http.Error(w, err.Error(), internalError)
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
